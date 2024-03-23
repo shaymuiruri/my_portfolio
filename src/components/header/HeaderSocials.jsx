043e978052cef35b1bfdf6dc3564f9051348d939
Mitchelle Muiruri <121823073+shaymuiruri@users.noreply.github.com>
2024-03-23 20:45:54 +0300

    update1 on assets

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+/*import {BsMedium} from 'react-icons/bs' */
+
+const HeaderSocials = () => {
+  return (
+    
+    <div className="header__socials">
+
+        <a href="https://www.linkedin.com/in/mitchelle-muiruri-477559269/" target="_blank" rel='noreferrer'><BsLinkedin/></a>
+        <a href="https://github.com/shaymuiruri" target="_blank" rel='noreferrer'><BsGithub/></a>
+    </div>
+  )
+}
+
+export default HeaderSocials

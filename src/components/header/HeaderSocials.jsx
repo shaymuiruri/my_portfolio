ce745f493beef06bebc3b9c781e92b05982e7d2f
Mitchelle Muiruri <121823073+shaymuiruri@users.noreply.github.com>
2024-04-01 12:39:11 +0300

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -8,8 +8,8 @@ const HeaderSocials = () => {
     
     <div className="header__socials">
 
-        <a href="https://www.linkedin.com/in/mitchelle-muiruri-477559269/" target="_blank" rel='noreferrer' style={{ marginRight: '20px'}} ><BsLinkedin/></a>
-        <a href="https://github.com/shaymuiruri" target="_blank" rel='noreferrer'><BsGithub/></a>
+{/*         <a href="https://www.linkedin.com/in/mitchelle-muiruri-477559269/" target="_blank" rel='noreferrer' style={{ marginRight: '20px'}} ><BsLinkedin/></a> */}
+{/*         <a href="https://github.com/shaymuiruri" target="_blank" rel='noreferrer'><BsGithub/></a> */}
     </div>
   )
 }
